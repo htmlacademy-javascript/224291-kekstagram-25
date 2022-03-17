65171c73f0c2d1f771ca18f0dce180582a42259f
Marina <marinal2303@yandex.ru>
2022-03-17 18:03:41 +0300

    итоговые_правки

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ const getRandom = function (min, max) {
     const rand = min - 0.5 + Math.random() * (max - min + 1);
     return Math.round(rand);
   } else {
-    throw new Error('Значение' + min 'должно быть больше' + max);
+    throw new Error(`Значение ${min} должно быть больше ${max}`);
   }
 };
 
