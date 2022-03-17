2e55682366806f81cdad49916cf51b4ce570997d
Marina <marinal2303@yandex.ru>
2022-03-17 16:15:27 +0300

    правки2

--- js/main.js
+++ js/main.js
@@ -4,24 +4,22 @@
 
 const MIN_VALUE = 1;
 const MAX_VALUE = 14;
+const STRING_COMMENT = 'Определенное количество символов';
+const LENGTH_LIMIT = 100;
 
-const GET_RANDOM = document function (min, max) {
+const getRandom = function (min, max) {
   if (min < max) {
-    const RAND = min - 0.5 + Math.random() * (max - min + 1);
-    return Math.round(RAND);
+    let rand = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(rand);
   } else {
     throw new Error('Значение ' + min + ' должно быть больше ' + max);
   }
 };
 
-GET_RANDOM(MIN_VALUE, MAX_VALUE);
+getRandom(MIN_VALUE, MAX_VALUE);
 
 
 // Функция для проверки максимальной длины строки.
-
-
-const STRING_COMMENT = 'Определенное количество символов';
-const LENGTH_LIMIT = 100;
 
 const RETURN_STRING_LENGTH = function (current, max) {
   return current.length <= max;
