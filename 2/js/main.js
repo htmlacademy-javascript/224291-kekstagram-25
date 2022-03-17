da141397fefc2f9e6d3ecdaaff65bbab2552d45d
Keks <keks@htmlacademy.ru>
2022-03-17 15:07:40 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,29 +1,31 @@
+
 // Функция, возвращающая случайное целое число из переданного диапазона включительно.
 // Источник https://learn.javascript.ru/task/random-int-min-max
 
-const minValue = 1;
-const maxValue = 14;
+const MIN_VALUE = 1;
+const MAX_VALUE = 14;
 
-const randomInteger = function (min, max) {
+const GET_RANDOM = document function (min, max) {
   if (min < max) {
-    const rand = min - 0.5 + Math.random() * (max - min + 1);
-    return Math.round(rand);
+    const RAND = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(RAND);
   } else {
-    alert('Значение ' + min + ' должно быть больше ' + max)
+    throw new Error('Значение ' + min + ' должно быть больше ' + max);
   }
 };
 
-randomInteger(minValue, maxValue);
+GET_RANDOM(MIN_VALUE, MAX_VALUE);
 
 
 // Функция для проверки максимальной длины строки.
 
 
-const stringComment = 'Определенное количество символов';
-const lengthLimit = 100;
+const STRING_COMMENT = 'Определенное количество символов';
+const LENGTH_LIMIT = 100;
 
-const returnLengthString = function (current, max) {
+const RETURN_STRING_LENGTH = function (current, max) {
   return current.length <= max;
 };
 
-returnLengthString(stringComment, lengthLimit);
+RETURN_STRING_LENGTH(STRING_COMMENT, LENGTH_LIMIT);
+
