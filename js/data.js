0f9b6563e9326df7b532a6d9420c7cf2f520de2c
Marina <marinal2303@yandex.ru>
2022-04-13 17:18:48 +0300

    дубль_5

--- js/data.js
+++ js/data.js
@@ -1,10 +1,12 @@
-import {getRandom, MESSENGER, AMOUNTCOMMENT} from './utils.js';
+import {
+  getRandom,
+  MESSENGER,
+  AMOUNTCOMMENT
+} from './utils.js';
 
-class PhotoDescription
-{
+class PhotoDescription {
 
-  constructor(id, url, description, likes, comments)
-  {
+  constructor(id, url, description, likes, comments) {
 
     this.id = id;
     this.url = url;
@@ -14,11 +16,9 @@ class PhotoDescription
   }
 }
 
-class PhotoComment
-{
+class PhotoComment {
 
-  constructor(id, avatar, message, name)
-  {
+  constructor(id, avatar, message, name) {
 
     this.id = id;
     this.avatar = avatar;
@@ -27,16 +27,14 @@ class PhotoComment
   }
 }
 
-const generatePhotoComment = function(count)
-{
+const generatePhotoComment = function (count) {
 
   //Создаем массив для хранения комментариев
   const comments = new Array();
 
-  for(let i = 1; i <= count; i++)
-  {
+  for (let i = 1; i <= count; i++) {
 
-    const message = MESSENGER[getRandom(0, 6)];  //Создаем аватар
+    const message = MESSENGER[getRandom(0, 6)]; //Создаем аватар
     const avatar = `img/avatar-${getRandom(1, 6)}.sv`;
     const photoComment = new PhotoComment(i, avatar, message, 'Марина');
 
@@ -45,12 +43,9 @@ const generatePhotoComment = function(count)
   return comments;
 };
 
-const generatePhotoDescription = function()
-{
-
- const photos = new Array();
-  for (let i = 1; i <= AMOUNTCOMMENT; i++)
-  {
+const generatePhotoDescription = function () {
+  const photos = new Array();
+  for (let i = 1; i <= AMOUNTCOMMENT; i++) {
     const url = `photos/${i}.jpg`; // Создаем url
     const description = 'Good';
     const likes = getRandom(15, 200);
@@ -67,6 +62,6 @@ const generatePhotoDescription = function()
 
 generatePhotoDescription();
 
-export {generatePhotoDescription};
-
-
+export {
+  generatePhotoDescription
+};
